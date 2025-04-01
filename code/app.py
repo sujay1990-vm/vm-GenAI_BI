5f8750e6a9f72cbb2ceab60349476002de9832fc
sujay1990-vm <sujay_nagvekar@valuemomentum.com>
2025-03-31 22:33:26 -0500

    final response changed

--- code/app.py
+++ code/app.py
@@ -730,7 +730,7 @@ if st.session_state["final_state"]:
     final_state = st.session_state["final_state"]
 
     st.subheader("Answer :")
-    nl_response = final_state.get("nl_response", "No response generated.")
+    nl_response = final_state.get("final_response", "No response generated.")
     st.markdown(f"<div style='font-size: 1.5em; font-weight: bold; color: #FFFFFF;'>{nl_response}</div>", unsafe_allow_html=True)
 
     with st.expander("Show SQL Queries Executed"):
